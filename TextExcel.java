0779cebb1751c989b07ff8f037209c0458fd823e
Mysteryman33 <139012459+Mysteryman33@users.noreply.github.com>
2026-06-01 10:10:14 -0700

    Add files via upload
    I uploaded them WITHOUT AI

---
+++ TextExcel.java
@@ -0,0 +1,12 @@
+
+// Update this file with your own code.
+
+public class TextExcel
+{
+
+	public static void main(String[] args)
+	{
+		Grid sheet = new Spreadsheet(); // Keep this as the first statement in main
+		// TODO finish implementing main by adding your own code here
+	}
+}
