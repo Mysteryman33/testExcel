f1a59af3383ea75fed65c108051ec8fdb015d3d7
whackemowen <owen.zhengzhiyun@gmail.com>
2026-06-08 10:24:58 -0700

    aravlikesme

--- Spreadsheet.java
+++ Spreadsheet.java
@@ -6,14 +6,18 @@ public class Spreadsheet implements Grid
 	private Cell[][] cells = new Cell[10][10];
 
 
-	public static boolean isInteger(String input) {
+	public static boolean isDouble(String str) {
+    if (str == null) {
+        return false;
+    }
     try {
-        Integer.parseInt(input);
-        return true; 
+        Double.parseDouble(str);
+        return true;
     } catch (NumberFormatException e) {
-        return false; 
+        return false;
     }
 }
+
 
 	@Override
 	public String processCommand(String command)
@@ -30,18 +34,27 @@ public class Spreadsheet implements Grid
 			} else {
 				String[] parts = command.split("=");
 				Location loc = new SpreadsheetLoc(parts[0].trim().toUpperCase());
-				if (isInteger(parts[1].trim())) {
-					cells[loc.getRow()-1][loc.getCol()] = new IntCell(Integer.parseInt(parts[1].trim()));
-					return "INT ITS AN INT!!!!!!";
-				} else {
-
-
-
+				if (isDouble(parts[1].trim())) {
+					cells[loc.getRow()-1][loc.getCol()] = new IntCell(Double.parseDouble(parts[1].trim()));
+					return "INT ITS AN INT welll its technically a double!!!!!!";
+				} else if (command.contains("%")) {
+					parts[1] = parts[1].replaceAll("%", "");
+					cells[loc.getRow()-1][loc.getCol()] = new PercentCell(Double.parseDouble(parts[1].trim()));
+				}
+				
+				else {
 				cells[loc.getRow()-1][loc.getCol()] = new TextCell(parts[1].trim());
 				return "Got it! Personal Owen Excel Manager At Your Service \n";}
 		} 
 		} else if (command.equals("clear")) {
 			cells = new Cell[10][10];
+		} else {
+			Location loc = new SpreadsheetLoc(command.trim().toUpperCase());
+			Cell c = getCell(loc);
+			if (c == null) {
+				return "Cell is empty";
+			}
+			return c.fullCellText();
 		}
 		// TODO Auto-generated method stub
 		return "";
